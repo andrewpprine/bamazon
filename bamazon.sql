7c22add5648fd158bc3b6674fcbf4c74fa586016
Andrew Prine <andrewpprine@gmail.com>
2018-08-22 23:27:19 -0700

    table data

--- bamazon.sql
+++ bamazon.sql
@@ -11,3 +11,14 @@ CREATE TABLE products (
    stock_quantity INT NOT NULL,
    PRIMARY KEY (id)
 );
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) VALUES 
+(11111, "mustard", "condiments", 2.00, 27),
+(22222, "roast beef", "meats", 6.49, 118),
+(33333, "pickles", "vegetables", 0.70, 65),
+(44444, "wheat", "breads", 3.00, 116),
+(55555, "onions", "vegetables", 0.85, 137),
+(66666, "mayonnaise", "condiments", 1.95, 31),
+(77777, "turkey", "meats", 5.99, 124), 
+(88888, "tomatoes", "vegetables", 0.90, 71),
+(99999, "dutch crunch", "breads", 3.25, 101);
