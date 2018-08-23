c85d2068a9a99ee92bee4ebbc22f87d343685968
Andrew Prine <andrewpprine@gmail.com>
2018-08-22 23:11:32 -0700

    basic table structure

---
+++ bamazon.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS bamazon_db;
+CREATE DATABASE bamazon_db;
+USE bamazon_db;
+
+CREATE TABLE products (
+   id INT NOT NULL AUTO_INCREMENT,
+   item_id INT NOT NULL,
+   product_name VARCHAR(255) NOT NULL,
+   department_name VARCHAR(255) NOT NULL,
+   price DECIMAL(10,2) NOT NULL,
+   stock_quantity INT NOT NULL,
+   PRIMARY KEY (id)
+);
