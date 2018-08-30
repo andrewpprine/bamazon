7543ac1f84d2ecd098524f0378f2ee00786cbd73
Andrew Prine <andrewpprine@gmail.com>
2018-08-30 15:49:29 -0700

    pseudocoding quantity prompt and purchasing

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,16 +10,6 @@ var connection = mysql.createConnection({
    database: "bamazon_db"
 });
 
-// connection.connect(function(err){
-//    if(err){
-//        throw err; 
-   
-//    else{
-//        console.log("connected as " + connection.threadId);
-//        loadProducts();
-//    }
-// });
-
 // Creates the connection with the server and loads the product data upon a successful connection
 connection.connect(function(err) {
    if (err) {
@@ -43,13 +33,77 @@ function promptForItem(inventory){
          {
             type: "input",
             name: "choice",
-            message: "What is the id of the item you want to purchase? [quit with Q]",
+            message: "What is the ID of the item you want to purchase? [quit with Q]",
             validate: function(val){
                return !isNaN(val)||val.toLowerCase()==="q"
             }
          }
-      ])
-      .then(function(val){
-         checkIfShouldExit(val.choice)
-      })
-}
+      ]).then(function(val){
+         checkIfShouldExit(val.choice);
+         var choiceID = parseInt(val.choice);
+         var product = checkInventory(choiceID,inventory);
+
+         // If there is a product with the id the user chose, prompt the customer for a desired quantity
+         if (product) {
+            // Pass the chosen product to promptCustomerForQuantity
+            promptCustomerForQuantity(product);
+         }
+         else {
+            // Otherwise let them know the item is not in the inventory, re-run loadProducts
+            console.log("\nThat item is not in the inventory.");
+            loadProducts();
+         }
+      });
+ }
+
+ // Prompt the customer for a product quantity
+function promptCustomerForQuantity(product) {}
+
+
+// Purchase the desired quantity of the desired item
+function makePurchase(product, quantity) {
+   connection.query(
+     "UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?",
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Check to see if the product the user chose exists in the inventory
+function checkInventory(choiceId, inventory) {
+   for (var i = 0; i < inventory.length; i++) {
+     if (inventory[i].item_id === choiceId) {
+       // If a matching product is found, return the product
+       return inventory[i];
+     }
+   }
+   // Otherwise return null
+   return null;
+ }
+ 
+ // Check to see if the user wants to quit the program
+ function checkIfShouldExit(choice) {
+   if (choice.toLowerCase() === "q") {
+     // Log a message and exit the current node process
+     console.log("Goodbye!");
+     process.exit(0);
+   }
+ }
