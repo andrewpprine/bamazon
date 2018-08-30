d867f36ed468c5ba4a734b315a7d8b17ab61ee63
Andrew Prine <andrewpprine@gmail.com>
2018-08-30 11:56:28 -0700

    load products and prompt for items

--- bamazon.sql
+++ bamazon.sql
@@ -3,22 +3,21 @@ CREATE DATABASE bamazon_db;
 USE bamazon_db;
 
 CREATE TABLE products (
-   id INT NOT NULL AUTO_INCREMENT,
-   item_id INT NOT NULL,
+   item_id INT AUTO_INCREMENT NOT NULL,
    product_name VARCHAR(255) NOT NULL,
    department_name VARCHAR(255) NOT NULL,
    price DECIMAL(10,2) NOT NULL,
    stock_quantity INT NOT NULL,
-   PRIMARY KEY (id)
+   PRIMARY KEY (item_id)
 );
 
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) VALUES 
-(11111, "mustard", "condiments", 2.00, 27),
-(22222, "roast beef", "meats", 6.49, 118),
-(33333, "pickles", "vegetables", 0.70, 65),
-(44444, "wheat", "breads", 3.00, 116),
-(55555, "onions", "vegetables", 0.85, 137),
-(66666, "mayonnaise", "condiments", 1.95, 31),
-(77777, "turkey", "meats", 5.99, 124), 
-(88888, "tomatoes", "vegetables", 0.90, 71),
-(99999, "dutch crunch", "breads", 3.25, 101);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES 
+("mustard", "condiments", 2.00, 27),
+("roast beef", "meats", 6.49, 118),
+("pickles", "vegetables", 0.70, 65),
+("wheat", "breads", 3.00, 116),
+("onions", "vegetables", 0.85, 137),
+("mayonnaise", "condiments", 1.95, 31),
+("turkey", "meats", 5.99, 124), 
+("tomatoes", "vegetables", 0.90, 71),
+("dutch crunch", "breads", 3.25, 101);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require('inquirer');
+require("console.table"); //formats data into table
 
 var connection = mysql.createConnection({
    host: "localhost",
@@ -9,13 +10,46 @@ var connection = mysql.createConnection({
    database: "bamazon_db"
 });
 
-connection.connect(function(err){
-   if(err){
-       throw err; 
+// connection.connect(function(err){
+//    if(err){
+//        throw err; 
+   
+//    else{
+//        console.log("connected as " + connection.threadId);
+//        loadProducts();
+//    }
+// });
+
+// Creates the connection with the server and loads the product data upon a successful connection
+connection.connect(function(err) {
+   if (err) {
+     console.error("error connecting: " + err.stack);
    }
-   else{
-       console.log("connected as " + connection.threadId);
-       runApp();
-   }
-});
-       
+   loadProducts();
+ });
+
+function loadProducts(){
+   connection.query("SELECT * FROM products", function(err,response){
+      if(err) throw err;
+      console.table(response);
+
+      promptForItem(); 
+   });
+}
+
+function promptForItem(inventory){
+   inquirer
+      .prompt([
+         {
+            type: "input",
+            name: "choice",
+            message: "What is the id of the item you want to purchase? [quit with Q]",
+            validate: function(val){
+               return !isNaN(val)||val.toLowerCase()==="q"
+            }
+         }
+      ])
+      .then(function(val){
+         checkIfShouldExit(val.choice)
+      })
+}
