4a1ecb39c0858fbeb00803e7f6ad2612d7b9db4c
Andrew Prine <andrewpprine@gmail.com>
2018-08-29 23:20:24 -0700

    mysql connection

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,10 @@
 var mysql = require("mysql");
 var inquirer = require('inquirer');
+
+var connection = mysql.createConnection({
+   host: "localhost",
+   port: 3306,
+   user: "root",
+   password: "hellosql",
+   database: "bamazon_db"
+});
